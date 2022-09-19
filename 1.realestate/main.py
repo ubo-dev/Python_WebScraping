de20e5936bae2d1f2ae8700e99f99aad95d467a7
Umut Bilal Okur <umutokur11@outlook.com>
2022-09-19 14:52:25 +0300

    up

---
+++ 1.realestate/main.py
@@ -0,0 +1,40 @@
+from collections import Counter
+from bs4 import BeautifulSoup
+import requests
+from csv import writer
+
+i = 0
+counter = 2
+url = "https://www.pararius.com/apartments/amsterdam"
+isHeader = True
+if counter < 20:
+  for i in range(18):
+    page = requests.get(url)
+    url = "https://www.pararius.com/apartments/amsterdam/page-"+str(counter)
+    counter += 1
+    soup = BeautifulSoup(page.content,'html.parser')
+    lists = soup.find_all('section',class_='listing-search-item')
+
+    with open('housing.csv', 'a', encoding='utf8', newline='') as f:
+      thewriter = writer(f)
+      if isHeader == True:
+        header = ['Title','Location','Price','Area','Number of Rooms','Interior']
+        thewriter.writerow(header)
+        isHeader = False
+      for list in lists:
+        title = list.find('a', class_="listing-search-item__link--title").text.replace('\n','')
+        location = list.find('div', class_="listing-search-item__sub-title").text.replace('\n','')
+        price = list.find('div', class_="listing-search-item__price").text.replace('\n','')
+        area = list.find('li', class_="illustrated-features__item illustrated-features__item--surface-area").text
+        number_of_rooms = list.find('li', class_="illustrated-features__item illustrated-features__item--number-of-rooms").text
+        try:
+          interior = list.find('li', class_="illustrated-features__item illustrated-features__item--interior").text
+        except:
+          continue
+
+        info = [title,location,price,area,number_of_rooms,interior]
+        thewriter.writerow(info)
+
+
+
+
---
+++ README.md
@@ -0,0 +1,12 @@
+# Python Web Scraping
+
+#I will be uploading my web scraping projects with python and liabriaries such as beautifulsoup4 and selenium.
+
+
+1. I used beautifulsoup4 in real estate web scraping.
+  - Output is written in csv format.
+    - Link of the source page I used = https://www.pararius.com/apartments/amsterdam
+  
+2. I tried to use beautifulsoup4 and selenium at the same time in my youtube web scraping project.
+  - Output is written in csv format.
+    - Link of the youtube page = https://www.youtube.com/user/jacksfilms
